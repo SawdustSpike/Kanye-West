a49f5a2f2b17619435a7b0c6b05c25fc3ffd394a
Mike Cowell <spike@spikeandbetty.com>
2022-11-18 15:09:15 -0500

    conversation complete.

--- Program.cs
+++ Program.cs
@@ -1,1 +1,18 @@
-﻿
+﻿using Newtonsoft.Json.Linq;
+
+var client = new HttpClient();
+var kanyeURL = "https://api.kanye.rest/";
+var swansonURL = "https://ron-swanson-quotes.herokuapp.com/v2/quotes";
+var kanyeResponse = client.GetStringAsync(kanyeURL).Result;
+var kanyeQuote = JObject.Parse(kanyeResponse).GetValue("quote").ToString();
+var ronResponse = client.GetStringAsync(swansonURL).Result;
+var ronQuote = JArray.Parse(ronResponse).ToString().Replace('[', ' ').Replace(']', ' ').Trim();
+for(int i =0; i < 10; i++)
+{
+    Console.WriteLine(ronQuote);
+    Console.WriteLine($"\"{kanyeQuote}\"");
+    kanyeResponse = client.GetStringAsync(kanyeURL).Result;
+    kanyeQuote = JObject.Parse(kanyeResponse).GetValue("quote").ToString();
+    ronResponse = client.GetStringAsync(swansonURL).Result;
+    ronQuote = JArray.Parse(ronResponse).ToString().Replace('[', ' ').Replace(']', ' ').Trim();
+}
